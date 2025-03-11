f28c4215899ea11a73e49bda9487ccb8e9b1ad01
fui96 <kidwelqn@mail.uc.edu>
2025-03-10 20:09:26 -0400

    reflowed layout

--- src/InvoiceFrame.java
+++ src/InvoiceFrame.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class InvoiceFrame extends JFrame {
 
     //Panels
-    JPanel MainPanel, TitlePanel, LineItemPanel,ProductPanel,CustomerPanel,InvoicePanel;
+    JPanel LeftPanel,MainPanel,TitlePanel, LineItemPanel,ProductPanel,CustomerPanel,InvoicePanel;
     //Buttons
     JButton QuitButton,AddLineItemButton,CreateInvoiceButton,AddCustInfo;
     //TextArea
@@ -18,7 +18,7 @@ public class InvoiceFrame extends JFrame {
     //ScrollPane
     JScrollPane InvoiceScrollPane;
     //JLabel
-    JLabel Title,ProductNameField,ProductPriceField,ProductQuantityField;
+    JLabel Title,ProductNameField,ProductPriceField,ProductQuantityField,CustomerNameField,CustomerAddressField;
 
     Toolkit tk = Toolkit.getDefaultToolkit();
     Dimension screenSize = tk.getScreenSize();
@@ -30,8 +30,12 @@ public class InvoiceFrame extends JFrame {
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
-        MainPanel = new JPanel();
-        add(MainPanel, BorderLayout.CENTER);
+        LeftPanel = new JPanel();
+        add(LeftPanel, BorderLayout.WEST);
+        CreateLineItemPanel();
+        LeftPanel.add(LineItemPanel, BorderLayout.NORTH);
+
+
 
 
         setVisible(true);
@@ -41,7 +45,10 @@ public class InvoiceFrame extends JFrame {
         LineItemPanel = new JPanel();
         LineItemPanel.setLayout(new GridLayout(2,1));
         LineItemPanel.setBorder(new TitledBorder(new EtchedBorder(),"Edit Line Items"));
-
+        CreateCustomerPanel();
+        CreateProductPanel();
+        LineItemPanel.add(CustomerPanel);
+        LineItemPanel.add(ProductPanel);
 
 
 
@@ -50,32 +57,53 @@ public class InvoiceFrame extends JFrame {
         //Panel Creation
         ProductPanel = new JPanel();
         ProductPanel.setLayout(new GridLayout(2,3));
-        ProductPanel.setBorder(new TitledBorder(new EtchedBorder(),"Edit Products"));
-        //Text Fields and Labels
+        ProductPanel.setBorder(new TitledBorder(new EtchedBorder(),"Edit Product"));
+        //Text Fields, Labels, and Button
         ProductName = new JTextField();
         ProductPrice = new JTextField();
-        ProductQuantity = new JSpinner(new SpinnerNumberModel(0,1,100,1));
+        ProductQuantity = new JSpinner(new SpinnerNumberModel(1,1,100,1));
         ProductNameField = new JLabel("Product Name");
         ProductPriceField = new JLabel("Product Price");
         ProductQuantityField = new JLabel("Product Quantity");
+        AddLineItemButton = new JButton("Add Line Item");
         //Add to Panel
         ProductPanel.add(ProductNameField);
         ProductPanel.add(ProductPriceField);
         ProductPanel.add(ProductQuantityField);
+        ProductPanel.add(AddLineItemButton);
         ProductPanel.add(ProductName);
         ProductPanel.add(ProductPrice);
         ProductPanel.add(ProductQuantity);
-
     }
     public void CreateCustomerPanel() {
+        //Panel Creation
         CustomerPanel = new JPanel();
+        CustomerPanel.setLayout(new GridLayout(2,3));
+        CustomerPanel.setBorder(new TitledBorder(new EtchedBorder(),"Edit Customer"));
+        //TextFields,Labels,and Button
+        CustAddress = new JTextField();
+        CustName = new JTextField();
+        CustomerNameField = new JLabel("Customer Name");
+        CustomerAddressField = new JLabel("Customer Address");
+        AddCustInfo = new JButton("Add Customer Info");
+        //Add to Panel
+        CustomerPanel.add(CustomerNameField);
+        CustomerPanel.add(CustomerAddressField);
+        CustomerPanel.add(AddCustInfo);
+        CustomerPanel.add(CustName);
+        CustomerPanel.add(CustAddress);
     }
 
     public void CreateInvoicePanel() {
-
+        InvoicePanel = new JPanel();
+        InvoicePanel.setLayout(new GridLayout(2,3));
     }
     public void CreateTitlePanel() {
 
     }
+    /*
+    public void Create
+
+     */
 
 }
